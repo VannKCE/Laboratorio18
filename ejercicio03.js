37b8c985fba2428e5732994051727d9c6ab0c94c
Vanessa <vcondorie@unsa.edu.pe>
2025-11-26 11:46:28 -0500

    AV18

---
+++ ejercicio03.js
@@ -0,0 +1,13 @@
+console.log("Ejercicio 03");
+function cargarUsuario() {
+    fetch("https://jsonplaceholder.typicode.com/users/10")
+        .then(response => response.json())
+        .then(usuario => {
+            console.log("Nombre:", usuario.name);
+            console.log("Username:", usuario.username);
+            console.log("Email:", usuario.email);
+        })
+        .catch(error => console.error("Error:", error));
+}
+
+cargarUsuario();
---
+++ ejercicio07.js
@@ -0,0 +1,11 @@
+console.log("Ejercicio 07");
+document.getElementById("btnUsuario").addEventListener("click", function () {
+    fetch("https://jsonplaceholder.typicode.com/users/2")
+        .then(response => response.json())
+        .then(usuario => {
+            document.getElementById("nombre").textContent = "Nombre: " + usuario.name;
+            document.getElementById("email").textContent = "Email: " + usuario.email;
+            document.getElementById("ciudad").textContent = "Ciudad: " + usuario.address.city;
+        })
+        .catch(error => console.error("Error:", error));
+});
